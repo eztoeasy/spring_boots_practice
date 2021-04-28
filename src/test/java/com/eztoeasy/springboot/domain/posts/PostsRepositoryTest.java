3109bce0d7f61cf937d92632e98431c81efbd5a5
eztoeasy <eztoeasy@hanmail.net>
2021-04-28 15:18:18 +0900

    feat : JPA Auditing 사용 1. Data 생성시간 / 수정시간 자동화 기능 구현 실습

--- src/test/java/com/eztoeasy/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/eztoeasy/springboot/domain/posts/PostsRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.eztoeasy.springboot.domain.posts;
 
 
+import org.apache.tomcat.jni.Local;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -39,5 +41,22 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록(){
+        LocalDateTime now = LocalDateTime.of(2019,6,4,0,0,0);
+        postsRepository.save(Posts.builder().title("title")
+        .content("content")
+        .author("author")
+        .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        System.out.println(">>>>> createDate=" + posts.getCreatedDate() + ", modifiedDate=" + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
